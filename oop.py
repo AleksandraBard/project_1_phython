b9e40d5e2ace3d6edc0ed4a4c55ebc844b4e29cf
Aleksandra Bardymova <bardymovasasha@gmail.com>
2021-03-08 11:47:41 -0500

    creating class object with Python

---
+++ oop.py
@@ -0,0 +1,27 @@
+my_student = {
+    'name': 'Sasha',
+    'grades': [70, 88, 90, 99, 88],
+    'average': None
+}
+
+def average_grade(student):
+    return sum(student['grades']) / len(student['grades'])
+
+
+#  Creating a class object
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student_one = Student("Bato", [90, 94, 87])
+
+
+print(student_one)
+print(student_one.__class__) # <class 'Student'>
+
